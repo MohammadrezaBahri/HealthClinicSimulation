745204ba2e871f5ffa121e881e207fc50287dabb
unknown <mdra.bahri.15@gmail.com>
2022-01-31 21:46:23 +0330

    test

--- src/state.py
+++ src/state.py
@@ -14,7 +14,7 @@ Step = 0
 Q3, Q2, Q1 = [], [], []
 all_patients = []
 NR, NS = 0, 0
-FEL, TR = [{h.Type: h.Arrival, h.Time: 0}, {h.Type: h.RestAlert, h.Time: 300}], [300]
+FEL = [{h.Type: h.Arrival, h.Time: 0}]
 SimulationEndTime = 28800 # minutes
 Replication = 100
 interarrival_time = 21 # exponential distribution mean
@@ -23,9 +23,9 @@ a, b = 1, 3 # beta parameters
 random_seed = 4
 
 def initialize():
-    global Time, Q3, Q2, Q1, NR, NS, FEL, TR
+    global Time, Q3, Q2, Q1, NR, NS, FEL
     Step = 0
     Time = 0
     Q3, Q2, Q1 = [], [], []
     NR, NS = 0, 0
-    FEL, TR = [{h.Type: h.Arrival, h.Time: 0}, {h.Type: h.RestAlert, h.Time: 300}], [300]
+    FEL, TR = [{h.Type: h.Arrival, h.Time: 0}]
